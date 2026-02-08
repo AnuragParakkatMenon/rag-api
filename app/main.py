d3f3201e9b9065355bba5c1543d15daf00666891
Anurag <anuragmenon94@gmail.com>
2026-02-09 02:35:26 +0530

    fix: adding https

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # Add this CORS middleware
 origins = [
     "http://localhost:5173",  # your dev frontend
-    "rag-ui-production.up.railway.app"  # production frontend
+    "https://rag-ui-production.up.railway.app/"  # production frontend
 ]
 
 app.add_middleware(
